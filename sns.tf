766ba4225f0ee629466564c7008429442e42346c
Zi'Aire Tirado <zi2jf33@gmail.com>
2026-09-06 01:41:46 -0400

    Delete sns.tf

--- sns.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_sns_topic" "alerts" {
-  name = "${local.name}-security-alerts"
-}
-
-resource "aws_sns_topic_subscription" "email" {
-  topic_arn = aws_sns_topic.alerts.arn
-  protocol  = "email"
-  endpoint  = var.alert_email
-}
